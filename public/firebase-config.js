eaaf8e7c5e4d34aadd7df001e8b08ce5715ba550
amcravila <amcravila@gmail.com>
2018-08-13 18:53:20 -0300

    Estrutura de arquivos

---
+++ public/firebase-config.js
@@ -0,0 +1,10 @@
+var config = {
+  apiKey: "AIzaSyBq47F4fpnjb3FTGFYuYvQ2m57bCmZR2qg",
+  authDomain: "good-sharing-95672.firebaseapp.com",
+  databaseURL: "https://good-sharing-95672.firebaseio.com",
+  projectId: "good-sharing-95672",
+  storageBucket: "good-sharing-95672.appspot.com",
+  messagingSenderId: "439197721736"
+};
+
+firebase.initializeApp(config);
